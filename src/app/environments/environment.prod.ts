048ce6dd66fc9475b89f83c3f16b896544beaa12
Olena Ivashchenko <olena.ivashchenko@techmagic.co>
2026-05-21 11:39:24 +0300

    removed envs

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  host: '',
-  googleClientId: '919824360149-35kgthvhkd57sgn48fhar1rjs7m9mf23.apps.googleusercontent.com',
-  googleClientSecret: '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'
-};
